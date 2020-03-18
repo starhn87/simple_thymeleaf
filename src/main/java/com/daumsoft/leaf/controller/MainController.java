41c15591b48d1f36b5065a7e3848286cb58e7d83
starhn87 <starhn87@gmail.com>
2020-03-18 10:34:39 +0900

    thymeleaf dialect 적용

--- src/main/java/com/daumsoft/leaf/controller/MainController.java
+++ src/main/java/com/daumsoft/leaf/controller/MainController.java
@@ -2,6 +2,8 @@ package com.daumsoft.leaf.controller;
 
 import com.daumsoft.leaf.vo.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/user")
-    public String start(Model model) {
-        User user = new User("starhn87", "1234", "Lee Seungwoo", "web");
-        model.addAttribute("user", user);
-        System.out.println(model);
+    public String start() {
+        // User user = new User("starhn87", "1234", "Lee Seungwoo", "web");
+        // model.addAttribute("user", user);
+        logger.info("start!!!");
         return "test";
     }
 }
