b8e4d1bac3a8b2ac908291c235bcaab508735299
이승우 <starhn87@gmail.com>
2020-03-24 13:43:00 +0900

    커밋 테스트

--- src/main/java/com/daumsoft/leaf/controller/MainController.java
+++ src/main/java/com/daumsoft/leaf/controller/MainController.java
@@ -15,8 +15,6 @@ public class MainController {
 
     @GetMapping("/user")
     public String start(Model model) {
-        User user = new User("starhn87", "1234", "Lee Seungwoo", "web");
-        model.addAttribute("user", user);
         logger.info("start!!!");
         return "test";
     }
